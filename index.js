79aa6c8608eabefebded4a42e5b9419af74d20db
Syed Wali Uddin Quadri <waliwasser29@gmail.com>
2022-12-12 20:29:08 +0530

    updated origins

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://pixilit.onrender.com",
         methods: ["GET","POST"],
     },
 });
